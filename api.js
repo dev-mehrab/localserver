e4498f3da82c35f13abb3354f61942381e1e1d36
dev-mehrab <devmehrab71@gmail.com>
2022-07-30 16:35:05 +0600

    added get & post method

---
+++ api.js
@@ -0,0 +1,29 @@
+const users = [
+    {
+        Name:"Mehrab",
+        Email: "mehrab@gmail.com",
+        Pass: 123456
+    },
+    {
+        Name:"hosen",
+        Email: "hosen@gmail.com",
+        Pass: 8745126
+    },
+    {
+        Name:"Ruble",
+        Email: "ruble@gmail.com",
+        Pass: 457895
+    },
+    {
+        Name:"Monira",
+        Email: "monira@gmail.com",
+        Pass: 12355
+    },
+    {
+        Name:"Medam",
+        Email: "medam@gmail.com",
+        Pass: 4579842
+    },
+];
+
+module.exports =users;
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const users = require('./api');
 
 const express = require("express");
  const app = express();
@@ -6,8 +7,19 @@ const express = require("express");
  app.use(express.json());
 
  app.get("/", (req, res) =>{
-    res.status(200).json("I am expresJeson mehrab");
+
+    res.status(200).json(users);
+ });
+
+ app.post("/user", (req, res) =>{
+   const createUser = req.body;
+     const newUser = users.push(createUser);
+      res.status(200).json({message:"Create user successfully", createUser});
+
+   
  })
+
+
 
  const port =process.env.port;
  app.listen(port,()=>{
